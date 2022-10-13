cff67a322981cc47d7ec9977f760a67eeaab9788
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-13 11:56:43 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import ColorBox from "./components/ColorBox.vue";
+
+const foregrounds = ["#E9E9E9", "#FF2020", "#ECFF", "#2BF"];
+const backgrounds = ["#111", "#222", "#333", "#444"];
+</script>
+
+<template>
+  <div class="grid">
+    <div
+      v-for="backgroundColor in backgrounds"
+      :key="backgroundColor"
+      class="row"
+    >
+      <ColorBox
+        v-for="foregroundColor in foregrounds"
+        :key="foregroundColor"
+        :background="backgroundColor"
+        :foreground="foregroundColor"
+        class="box"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-rows: 1fr;
+  gap: 10px;
+}
+
+.row {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ src/components/ColorBox.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { hex } from "wcag-contrast";
+
+const props = defineProps(["background", "foreground"]);
+const contrastValue = hex(props.background, props.foreground);
+</script>
+
+<template>
+  <div>{{ contrastValue.toFixed(2) }}</div>
+</template>
+
+<style scoped>
+div {
+  height: 200px;
+  width: 200px;
+  background-color: v-bind("props.background");
+  color: v-bind("props.foreground");
+  display: grid;
+  place-content: center;
+}
+</style>

ff70d5b88b54627b9eb46daeaa90f9e3e8688591
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-14 18:46:43 -0300

    Remove pinia and refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,17 @@
 <script setup>
-import ColorGrid from "./components/ColorGrid.vue";
-import { useColorStore } from "./stores/colors";
-import ColorsPalette from "./components/ColorsPalette.vue";
+import VueColorInput from "vue-color-input";
+import { useStorage } from "@vueuse/core";
 
-const colorStore = useColorStore();
+const foregroundColors = useStorage("foregroundColors", []);
 </script>
 
 <template>
-  <ColorsPalette
-    :colorList="colorStore.backgroundColors"
-    :defaultColor="'#E8E8E8'"
-  />
-  <ColorsPalette
-    :colorList="colorStore.foregroundColors"
-    :defaultColor="'#151515'"
-  />
-  <ColorGrid />
-  <!-- 
-  <div v-for="color in backgrounds" :key="color">
-    {{ color }}
-    <input type="color" id="favColor" v-model="color.value" />
-    <input type="text" id="colorCode" v-model="color.value" />
-    <button @click="removeBackground(color)">remove me</button>
-    <button @click="copyBackground(color.value)"><file-copy-fill /></button>
-
-
-  <div class="grid">
-    <div
-      v-for="backgroundColor in backgrounds"
-      :key="backgroundColor"
-      class="row"
-    >
-      <ColorBox
-        v-for="foregroundColor in foregrounds"
-        :key="foregroundColor"
-        :background="backgroundColor.value"
-        :foreground="foregroundColor"
-        class="box"
-      />
-    </div>
-  </div> -->
+  <div v-for="(color, index) in foregroundColors" :key="index">
+    <VueColorInput v-model="foregroundColors[index]" />
+    {{ foregroundColors[index] }}
+    <button @click="foregroundColors.splice(index, 1)">del me</button>
+  </div>
+  <button @click="foregroundColors.push('#fff')">new color</button>
 </template>
 
-<style scoped>
-.grid {
-  display: grid;
-  grid-template-rows: 1fr;
-  gap: 10px;
-}
-
-.row {
-  display: flex;
-  gap: 10px;
-}
-</style>
+<style scoped></style>

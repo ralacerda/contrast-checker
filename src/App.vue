479d5140294cf691e4214f856262b9c7eea9726c
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-25 21:48:22 -0300

    New v-for loop using templates

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,45 @@
 <script setup>
-import ColorInput from "vue-color-input";
 import { useStorage } from "@vueuse/core";
 import ColorBox from "./components/ColorBox.vue";
+import ColorButton from "./components/ColorButton.vue";
 
-const foregroundColors = useStorage("foregroundColors", []);
+const foregroundColors = useStorage("foregroundColors", ["#FFF", "#e4e4e4"]);
 const backgroundColors = useStorage("backgroundColors", []);
 </script>
 
 <template>
-  <div class="color-palettes">
-    <div class="foreground-buttons">
-      <div v-for="(color, index) in foregroundColors" :key="index">
-        <ColorInput v-model="foregroundColors[index]" />
-        <input v-model="foregroundColors[index]" />
-        <button @click="foregroundColors.splice(index, 1)">del me</button>
-      </div>
-      <button @click="foregroundColors.push('#fff')">new color</button>
-    </div>
+  <ColorButton
+    v-for="(color, index) in backgroundColors"
+    :key="index"
+    :modelValue="backgroundColors[index]"
+    @update:modelValue="(x) => (backgroundColors[index] = x)"
+    @delete="backgroundColors.splice(index, 1)" />
+  <button @click="backgroundColors.push('#fff')">new color</button>
 
-    <div class="background-buttons">
-      <div v-for="(color, index) in backgroundColors" :key="index">
-        <ColorInput v-model="backgroundColors[index]" />
-        <input v-model="backgroundColors[index]" />
-        <button @click="backgroundColors.splice(index, 1)">del me</button>
-      </div>
-      <button @click="backgroundColors.push('#000')">new color</button>
-    </div>
-  </div>
+  <ColorButton
+    v-for="(color, index) in foregroundColors"
+    :key="index"
+    :modelValue="foregroundColors[index]"
+    @update:modelValue="(x) => (foregroundColors[index] = x)"
+    @delete="foregroundColors.splice(index, 1)" />
+  <button @click="foregroundColors.push('#fff')">new color</button>
 
   <div class="color-grid">
-    <div>
-      <div
-        v-for="(foreground, index) in foregroundColors"
-        :key="index"
-        class="background-columns"
-      >
-        {{ foreground }}
-      </div>
-    </div>
-    <div
-      v-for="(background, index) in backgroundColors"
-      :key="index"
-      class="background-columns"
-    >
+    <template v-for="(background, index) in backgroundColors" :key="index">
       {{ background }}
-      <div
-        v-for="(foreground, index) in foregroundColors"
-        :key="index"
-        class="foreground-rows"
-      >
+    </template>
+    <template v-for="(foreground, index) in foregroundColors" :key="index">
+      {{ foreground }}
+      <template v-for="(background, index) in backgroundColors" :key="index">
         <ColorBox :background="background" :foreground="foreground" />
-      </div>
-    </div>
+      </template>
+    </template>
   </div>
 </template>
 
 <style scoped>
 .background-buttons,
 .foreground-buttons {
-  display: flex;
-}
-
-.color-grid {
   display: flex;
 }
 </style>

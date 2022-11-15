ec548d7d26125509b8e3ae6b79ab2faa273c87b5
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-14 21:29:36 -0300

    Box wrapper

--- src/components/ColorBox.vue
+++ src/components/ColorBox.vue
@@ -5,11 +5,29 @@ const props = defineProps(["background", "foreground"]);
 </script>
 
 <template>
-  <div>{{ hex(background, foreground).toFixed(2) }}</div>
+  <div class="bigger-box">
+    <div class="combination-box">
+      {{ hex(background, foreground).toFixed(2) }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
 div {
+.bigger-box {
+  width: 120px;
+  height: 80px;
+  display: grid;
+  place-items: center;
+  border: solid 1px black;
+  border-radius: 4px;
+  margin: 10px;
+}
+
+.combination-box {
+  display: grid;
+  place-items: center;
+  font-size: 20px;
   height: 60px;
   width: 60px;
   background: v-bind("props.background");

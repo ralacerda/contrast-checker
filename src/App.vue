a12dbea3b9a18d6bf5af18d8dc0564bc0654a100
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-16 14:01:33 -0300

    Add label to options checkbox

--- src/App.vue
+++ src/App.vue
@@ -20,61 +20,65 @@ const options = useStorage(
 </script>
 
 <template>
-  <input
-    type="checkbox"
-    id="switch"
-    class="checkbox"
-    v-model="options.showShadows" />
-  <input
-    type="checkbox"
-    id="switch"
-    class="checkbox"
-    v-model="options.showBorders" />
-  <input
-    type="checkbox"
-    id="switch"
-    class="checkbox"
-    v-model="options.showPreview" />
-  <input
-    type="checkbox"
-    id="switch"
-    class="checkbox"
-    v-model="options.showText" />
+  <div class="box">
+    <label class="checkbox">
+      <input type="checkbox" v-model="options.showShadows" />
+      Show colored shadows
+    </label>
 
-  <input
-    type="checkbox"
-    id="switch"
-    class="checkbox"
-    v-model="options.enhancedContrast" />
+    <label class="checkbox">
+      <input type="checkbox" v-model="options.showBorders" />
+      Show different border styles
+    </label>
 
-  <ColorButton
-    v-for="(color, index) in backgroundColors"
-    :key="index"
-    :modelValue="backgroundColors[index]"
-    @update:modelValue="(x) => (backgroundColors[index] = x)"
-    @delete="backgroundColors.splice(index, 1)" />
-  <button @click="backgroundColors.push('#fff')">new color</button>
+    <label class="checkbox">
+      <input type="checkbox" v-model="options.showPreview" />
+      Show colors preview
+    </label>
 
-  <ColorButton
-    v-for="(color, index) in foregroundColors"
-    :key="index"
-    :modelValue="foregroundColors[index]"
-    @update:modelValue="(x) => (foregroundColors[index] = x)"
-    @delete="foregroundColors.splice(index, 1)" />
-  <button @click="foregroundColors.push('#fff')">new color</button>
+    <label class="checkbox">
+      <input type="checkbox" v-model="options.showText" />
+      Show result in text
+    </label>
 
-  <div class="color-grid">
-    <template v-for="(background, index) in backgroundColors" :key="index">
-      {{ background }}
-    </template>
-    <template v-for="(foreground, index) in foregroundColors" :key="index">
-      {{ foreground }}
+    <label class="checkbox">
+      <input type="checkbox" v-model="options.enhancedContrast" />
+      Test enhanced contrast level
+    </label>
+  </div>
+
+  <div class="box">
+    <ColorButton
+      v-for="(color, index) in backgroundColors"
+      :key="index"
+      :modelValue="backgroundColors[index]"
+      @update:modelValue="(x) => (backgroundColors[index] = x)"
+      @delete="backgroundColors.splice(index, 1)" />
+    <button @click="backgroundColors.push('#fff')">new color</button>
+
+    <ColorButton
+      v-for="(color, index) in foregroundColors"
+      :key="index"
+      :modelValue="foregroundColors[index]"
+      @update:modelValue="(x) => (foregroundColors[index] = x)"
+      @delete="foregroundColors.splice(index, 1)" />
+    <button @click="foregroundColors.push('#fff')">new color</button>
+  </div>
+
+  <div class="box">
+    <div class="color-grid">
       <template v-for="(background, index) in backgroundColors" :key="index">
-        <ColorBox
-          :background="background"
-          :foreground="foreground"
-          :options="options" />
+        {{ background }}
       </template>
-    </template>
+      <template v-for="(foreground, index) in foregroundColors" :key="index">
+        {{ foreground }}
+        <template v-for="(background, index) in backgroundColors" :key="index">
+          <ColorBox
+            :background="background"
+            :foreground="foreground"
+            :options="options" />
+        </template>
+      </template>
+    </div>
   </div>
 </template>

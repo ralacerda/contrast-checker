b9c168c2b62a4fccde33e02cdc21df58dbb8774f
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-17 14:53:12 -0300

    Styles changes and better defaults

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,27 @@ import ColorButton from "./components/ColorButton.vue";
 import NewColor from "./components/NewColor.vue";
 import { reactive } from "vue";
 
-const foregroundColors = useStorage("foregroundColors", ["#FFF", "#e4e4e4"]);
-const backgroundColors = useStorage("backgroundColors", []);
+const foregroundColors = useStorage("foregroundColors", [
+  "#900498",
+  "#02513c",
+  "#114c72",
+  "#868686",
+  "#aaab8b",
+]);
+const backgroundColors = useStorage("backgroundColors", [
+  "#eeeeee",
+  "#e5ebec",
+  "#161414",
+]);
 
 const options = useStorage(
   "options",
   reactive({
     showShadows: true,
-    enhancedContrast: false,
-    showBorders: true,
     showPreview: true,
-    showText: true,
+    showBorders: false,
+    showText: false,
+    enhancedContrast: false,
   })
 );
 </script>
@@ -31,13 +41,13 @@ const options = useStorage(
       </label>
 
       <label class="checkbox">
-        <input type="checkbox" v-model="options.showBorders" />
-        Border styles
+        <input type="checkbox" v-model="options.showPreview" />
+        Preview
       </label>
 
       <label class="checkbox">
-        <input type="checkbox" v-model="options.showPreview" />
-        Colors preview
+        <input type="checkbox" v-model="options.showBorders" />
+        Border styles
       </label>
 
       <label class="checkbox">
@@ -53,15 +63,16 @@ const options = useStorage(
   </div>
 
   <div class="block">
-    <h2 class="title is-4 mb-2 mt-4">Backgrounds</h2>
-    <div class="button-row block">
-      <ColorButton
-        v-for="(color, index) in backgroundColors"
-        :key="index"
-        :modelValue="backgroundColors[index]"
-        @update:modelValue="(x) => (backgroundColors[index] = x)"
-        @delete="backgroundColors.splice(index, 1)" />
-      <NewColor @click="backgroundColors.push('#FFFFFF')" />
+    <div class="block">
+      <h2 class="title is-4 mb-2 mt-4">Backgrounds</h2>
+      <div class="button-row block">
+        <ColorButton
+          v-for="(color, index) in backgroundColors"
+          v-model="backgroundColors[index]"
+          :key="index"
+          @delete="backgroundColors.splice(index, 1)" />
+        <NewColor @click="backgroundColors.push('#FFFFFF')" />
+      </div>
     </div>
 
     <div class="block">
@@ -69,9 +80,8 @@ const options = useStorage(
       <div class="button-row block">
         <ColorButton
           v-for="(color, index) in foregroundColors"
+          v-model="foregroundColors[index]"
           :key="index"
-          :modelValue="foregroundColors[index]"
-          @update:modelValue="(x) => (foregroundColors[index] = x)"
           @delete="foregroundColors.splice(index, 1)" />
         <NewColor @click="foregroundColors.push('#000000')" />
       </div>
@@ -133,7 +143,11 @@ const options = useStorage(
   max-width: 30ch;
 }
 
-.block + .block {
+.block {
+  margin-bottom: 2rem;
+}
+
+#app > .block + .block {
   border-top: solid 1px #00000010;
 }
 </style>
